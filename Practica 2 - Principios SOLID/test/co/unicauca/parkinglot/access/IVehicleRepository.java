6f96d7a56352e72b1b6fa438a33f7d64a5a7ad88
Daniela Riascos <67487584+Daniela617@users.noreply.github.com>
2022-10-19 06:20:12 -0500

    Delete Practica 2 - Principios SOLID/test/co/unicauca/parkinglot/access directory

--- Practica 2 - Principios SOLID/test/co/unicauca/parkinglot/access/IVehicleRepository.java
+++
@@ -1,9 +0,0 @@
-package co.unicauca.parkinglot.access;
-
-import co.unicauca.parkinglot.domain.Vehicle;
-import java.util.List;
-
-public interface IVehicleRepository {
-    public boolean save(Vehicle prmNewVehiculo);
-    public List<Vehicle> list();
-}
