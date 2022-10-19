b997c58c2cdb71ea9a609825604650b380e46dc0
Daniela Riascos <67487584+Daniela617@users.noreply.github.com>
2022-10-19 06:20:22 -0500

    Delete Practica 2 - Principios SOLID/test/co/unicauca/parkinglot/infra directory

--- Practica 2 - Principios SOLID/test/co/unicauca/parkinglot/infra/Utilities.java
+++
@@ -1,9 +0,0 @@
-package co.unicauca.parkinglot.infra;
-
-public class Utilities {
-    public static long redondear(long prmValue){
-        double varValue = prmValue;
-        varValue = varValue / 100;
-        return (long) (Math.ceil(varValue) * 100);
-    }
-}
